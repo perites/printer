7188facb6702928b822075c96a69a19ca88cddf6
perite <nikita.krementar@gmail.com>
2023-11-08 21:47:11 +0100

    overall improvment, added ClientMessagesContoller and ServerMessagesController classes

--- client.py
+++ client.py
@@ -5,32 +5,37 @@ import time
 URL = "http://127.0.0.1:5000/"
 
 
-def show_message(message):
-    if isinstance(message, dict):
-        print(f'\n{message["sender"]} to {message["message_to"]}: {message["message"]} at {message["time"]}')
-    else:
-        print(message)
+class ClientMessagesContoller():
+    def __init__(self, name):
+        self.client_name = name
+
+    def show_message(self, message):
+        if isinstance(message, dict):
+            print(f'\n{message["message_from"]} to {message["message_to"]}: {message["message"]} at {message["time"]}')
+        else:
+            print(message)
+
+    def send_message(self):
+        while True:
+            message_to = input("message to :")
+            message = input("message : ")
+            message = {"time": time.ctime(), "message_from": self.client_name, "message": message, "message_to": message_to}
+            requests.post(URL, json=message)
+
+    def fetch_messages(self):
+        headers = {"message-to": self.client_name}
+        while True:
+            message = requests.get(URL, headers=headers)
+            if (message := message.json()) != "nnm":
+                self.show_message(message)
+            time.sleep(1)
+
+    def start_chatting(self):
+        send_message_thread = threading.Thread(target=self.send_message)
+        fetch_messages_thread = threading.Thread(target=self.fetch_messages)
+        send_message_thread.start()
+        fetch_messages_thread.start()
 
 
-def send_message(name):
-    while True:
-        message_to = input("message to :")
-        message = input("message : ")
-        message = {"time": time.ctime(), "sender": name, "message": message, "message_to": message_to}
-        requests.post(URL, json=message)
-
-
-def fetch_messages(name):
-    while True:
-        headers = {"message-to": name}
-        message = requests.get(URL, headers=headers)
-        if message and (message := message.json()) != "nnm":
-            show_message(message)
-        time.sleep(1)
-
-
-name = input("your name : ")
-send_message_thread = threading.Thread(target=send_message, args=(name,))
-fetch_messages_thread = threading.Thread(target=fetch_messages, args=(name,))
-send_message_thread.start()
-fetch_messages_thread.start()
+mc = ClientMessagesContoller(input("your name: "))
+mc.start_chatting()
--- server.py
+++ server.py
@@ -2,28 +2,30 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'c42e8d7a0a1003456342385cb9e30b6b'
 
-MESSAGES = []
+
+class ServerMessagesController():
+    def __init__(self):
+        self.all_messages = []
+
+    def store_message(self, message):
+        self.all_messages.append(message)
+
+    def find_message(self, message_to):
+        for message in self.all_messages:
+            if message["message_to"] == message_to:
+                self.all_messages.remove(message)
+                return message
+
+        return "nnm"
 
 
-def store_message(message):
-    global MESSAGES
-    MESSAGES.append(message)
-
-
-def find_message(message_to):
-    global MESSAGES
-    for message in MESSAGES:
-        if message["message_to"] == message_to:
-            MESSAGES.remove(message)
-            return message
-
-    return "nnm"
+mc = ServerMessagesController()
 
 
 @app.route("/", methods=["POST"])
 def get_messages():
     message = request.json
-    store_message(message)
+    mc.store_message(message)
 
     return "", 200
 
@@ -32,7 +34,7 @@ def get_messages():
 def give_messages():
     message_to = request.headers.get("message-to")
 
-    return jsonify(find_message(message_to))
+    return jsonify(mc.find_message(message_to))
 
 
 if __name__ == '__main__':
