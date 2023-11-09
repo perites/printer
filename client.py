654d80d0d9d45056eae13d90eeb0fd0675d09aee
perite <nikita.krementar@gmail.com>
2023-11-09 16:58:23 +0100

    overall improvment, added socalled terminal, command to it

--- client.py
+++ client.py
@@ -2,31 +2,47 @@ import threading
 import requests
 import time
 
+URL = "https://chat-test-k49y.onrender.com"
 URL = "http://127.0.0.1:5000/"
 
 
 class ClientMessagesContoller():
     def __init__(self, name):
         self.client_name = name
+        self.commads = ["help", "omembers", "chat"]
+        self.chat = False
 
     def show_message(self, message):
         if isinstance(message, dict):
-            print(f'\n{message["message_from"]} to {message["message_to"]}: {message["message"]} at {message["time"]}')
+            if message["system"]:
+                print(f'\n{message["message_from"]} to {message["message_to"]}:{message["message"]} at {message["time"]}')
+                return
+
+            print(f'\n{message["message_from"]} to {message["message_to"]}:{message["message"]} at {message["time"]}')
+            message = {"time": time.ctime(), "message_from": self.client_name, "message": f"have read your message:  {message['message']}", "message_to": message['message_from'], "system": True}
+            requests.post(URL, json=message)
         else:
-            print(message)
+            print("\n" + message)
 
     def send_message(self):
-        while True:
-            message_to = input("message to: ")
-            message = input("message: ")
-            message = {"time": time.ctime(), "message_from": self.client_name, "message": message, "message_to": message_to}
-            requests.post(URL, json=message)
+        message_to = input("chat with:")
+        while self.chat:
+            message = input("message:")
+            match message:
+                case "exit":
+                    self.chat = False
+                    return
+                case "":
+                    print("message cant be empty")
+                case _:
+                    message = {"time": time.ctime(), "message_from": self.client_name, "message": message, "message_to": message_to, "system": False}
+                    requests.post(URL, json=message)
 
     def fetch_messages(self):
         headers = {"message-to": self.client_name}
-        while True:
+        while self.chat:
             message = requests.get(URL, headers=headers)
-            if (message := message.json()) != "nnm":
+            if (message := message.json()):
                 self.show_message(message)
             time.sleep(1)
 
@@ -36,6 +52,25 @@ class ClientMessagesContoller():
         send_message_thread.start()
         fetch_messages_thread.start()
 
+    def terminal(self):
+        while not self.chat:
+            user_input = input("command:")
+            match user_input:
+                case "help":
+                    print(self.commads)
 
-mc = ClientMessagesContoller(input("your name: "))
-mc.start_chatting()
+                case "omembers":
+                    answer = requests.get(URL + "/omembers").json()
+                    print(f"Currently online {len(answer)} members:\n{answer}")
+
+                case "chat":
+                    self.chat = True
+                    self.start_chatting()
+
+                case _:
+                    print("wrong command")
+
+
+mc = ClientMessagesContoller(input("your name:"))
+while True:
+    mc.terminal()
--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+import time
+import threading
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'c42e8d7a0a1003456342385cb9e30b6b'
 
@@ -6,6 +8,7 @@ app.config["SECRET_KEY"] = 'c42e8d7a0a1003456342385cb9e30b6b'
 class ServerMessagesController():
     def __init__(self):
         self.all_messages = []
+        self.members_online = {}
 
     def store_message(self, message):
         self.all_messages.append(message)
@@ -16,13 +19,24 @@ class ServerMessagesController():
                 self.all_messages.remove(message)
                 return message
 
-        return "nnm"
+    def members_online_update(self):
+        while True:
+            items_to_remove = []
+
+            members_online_copy = self.members_online.copy()
+            for user, last_o in members_online_copy.items():
+                if time.time() - last_o > 10:
+                    items_to_remove.append(user)
+            for user in items_to_remove:
+                del self.members_online[user]
 
 
 mc = ServerMessagesController()
+members_online_update_thread = threading.Thread(target=mc.members_online_update)
+members_online_update_thread.start()
 
 
-@app.route("/", methods=["POST"])
+@ app.route("/", methods=["POST"])
 def get_messages():
     message = request.json
     mc.store_message(message)
@@ -30,11 +44,16 @@ def get_messages():
     return "", 200
 
 
-@app.route("/", methods=["GET"])
+@ app.route("/", methods=["GET"])
 def give_messages():
     message_to = request.headers.get("message-to")
-
+    mc.members_online[message_to] = time.time()
     return jsonify(mc.find_message(message_to))
+
+
+@ app.route("/omembers", methods=["GET"])
+def online_members():
+    return jsonify(list(mc.members_online.keys()))
 
 
 if __name__ == '__main__':
